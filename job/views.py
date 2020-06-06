45d9ac9899979faaec8157940f18d6aca7667cd7
hedra <hedraatia@gmail.com>
2020-06-06 06:45:49 -0800

    add temp,static,media

--- job/views.py
+++ job/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+#from .models import Job
+from .models import Job, Category
+
+def job_list(request):
+    job_list = Job.objects.all()
+    context ={'jobs' : job_list}  # template name
+    return render(request,'job/job_list.html', context)
+
+
+
+
+def job_detail(request, id):
+    job_detail = Job.objects.get(id=id)
+    context ={'job' : job_detail}  # template name
+    return render(request,'job/job_detail.html', context)
